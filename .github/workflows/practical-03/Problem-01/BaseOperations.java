c6a9ab8c697e6501941b3a403457fc71c0a5a74a
Crazyorchid <56853075+Crazyorchid@users.noreply.github.com>
2020-03-30 21:23:32 +1030

    Practical03

---
+++ .github/workflows/practical-03/Problem-01/BaseOperations.java
@@ -0,0 +1,25 @@
+public class BaseOperations {
+public static void reverseArray(int[]array) {
+		for (int i=array.length-1; i>=0; i--) {
+			System.out.println(array[i]);
+				}
+	}
+		
+		public static void reverseArray(float[]array) {
+			for (int i=array.length-1; i>=0; i--) {
+				System.out.println(array[i]);
+					}
+		}
+			public static void reverseArray(String[]array) {
+				for (int i=array.length-1; i>=0; i--) {
+					System.out.println(array[i]);
+						}
+
+			}
+		
+
+
+
+	}
+
+
